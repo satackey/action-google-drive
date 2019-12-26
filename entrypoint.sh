aecb9b40f4431db24de385ff2f94cc248a582eaa
satackey <21271711+satackey@users.noreply.github.com>
2019-12-26 23:36:01 +0900

    `$UPLOAD_FROM`を相対パスにした

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,10 @@ set -eu
 skicka init
 echo $SKICKA_TOKENCACHE_JSON > $HOME/.skicka.tokencache.json
 
-UPLOAD_FROM_ABSOLUTE="$GITHUB_WORKSPACE/$UPLOAD_FROM"
-
-skicka upload -ignore-times "$UPLOAD_FROM_ABSOLUTE" "$UPLOAD_TO"
+skicka upload -ignore-times "$UPLOAD_FROM" "$UPLOAD_TO"
 
 # Remove outdated
-skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM_ABSOLUTE" 2>&1 | \
+skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM" 2>&1 | \
     sed "/Downloaded and wrote/!d" | \
     sed -E "s/.*bytes to //" | \
-    xargs -I{} skicka rm "$UPLOAD_FROM_ABSOLUTE/{}" || true
+    xargs -I{} skicka rm "$UPLOAD_FROM/{}" || true
