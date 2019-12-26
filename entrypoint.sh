5886b377ade16246b69414f21031be611cd303cb
satackey <21271711+satackey@users.noreply.github.com>
2019-12-26 23:42:14 +0900

    戻した

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,12 @@ set -eu
 skicka init
 echo $SKICKA_TOKENCACHE_JSON > $HOME/.skicka.tokencache.json
 
-skicka upload -ignore-times "$UPLOAD_FROM" "$UPLOAD_TO"
+UPLOAD_FROM_ABSOLUTE="$GITHUB_WORKSPACE/$UPLOAD_FROM"
+
+skicka upload -ignore-times "$UPLOAD_FROM_ABSOLUTE" "$UPLOAD_TO"
 
 # Remove outdated
-skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM" 2>&1 | \
+skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM_ABSOLUTE" 2>&1 | \
     sed "/Downloaded and wrote/!d" | \
     sed -E "s/.*bytes to //" | \
-    xargs -I{} skicka rm "$UPLOAD_FROM/{}" || true
+    xargs -I{} skicka rm "$UPLOAD_FROM_ABSOLUTE/{}" || true
