6a4e9d22b2d186f9fa85b63e179cb48b57d50b5b
satackey <21271711+satackey@users.noreply.github.com>
2019-12-27 16:41:58 +0900

    remove-outdateオプションを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,11 @@ echo $SKICKA_TOKENCACHE_JSON > $HOME/.skicka.tokencache.json
 skicka upload -ignore-times "$UPLOAD_FROM" "$UPLOAD_TO"
 
 # Remove outdated
-skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM" 2>&1 | \
-    sed "/Downloaded and wrote/!d" | \
-    sed -E "s/.*bytes to //" | \
-    xargs -I{} skicka rm "$UPLOAD_FROM/{}" || true
+if [ $REMOVE_OUTDATED == "true" ]; then
+    skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM" 2>&1 | \
+        sed "/Downloaded and wrote/!d" | \
+        sed -E "s/.*bytes to //" | \
+        xargs -I{} skicka rm "$UPLOAD_FROM{}" || true
+elif [ $REMOVE_OUTDATED != "false" ]; then
+    exit 1
+fi
